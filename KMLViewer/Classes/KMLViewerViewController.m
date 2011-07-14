a076f6d227aadd711d237d72a92f48147c9fc82e
Tobias Carlander <tobias.carlander@wfp.org>
2011-07-14 16:27:51 +0400

    Fixing KML

--- KMLViewer/Classes/KMLViewerViewController.m
+++ KMLViewer/Classes/KMLViewerViewController.m
@@ -58,8 +58,8 @@
     // Locate the path to the route.kml file in the application's bundle
     // and parse it with the KMLParser.
     // TODO:  Echange this for online version http://87.201.216.107/trackme/kml.kml
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"route" ofType:@"kml"];
-    kml = [[KMLParser parseKMLAtPath:path] retain];
+    NSURL *path = [NSURL URLWithString:@"http://87.201.216.107/trackme/kml/paths.kml"] ;
+    kml = [[KMLParser parseKMLAtURL:path]retain];
     
     // Add all of the MKOverlay objects parsed from the KML file to the map.
     NSArray *overlays = [kml overlays];
